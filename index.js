f526473aeb931d4358bb434a6e0160c511ba0eef
nmahler1240 <81329454+nmahler1240@users.noreply.github.com>
2021-05-22 23:41:17 +0200

    Changes

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-   var element = document.body;
-   element.classList.toggle("dark-mode");
-}
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
